ab6f46ab60e68deaa41b9d57f179802d6cdb6d69
Maurice Kelly <kay86@live.com>
2021-07-08 20:40:50 +0100

    Add ContactEntity

--- Lib_Shared/Domain/ToDoItem.cs
+++ Lib_Shared/Domain/ToDoItem.cs
@@ -10,4 +10,21 @@ namespace Domain
         public string Title { get; set; } // Title => Entry
         public bool IsDone { get; set; }
     }
+
+    // Todo: Contact Manager | Entry
+    public class ContactEntity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        //public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string HomeAddress { get; set; }
+        public string Image { get; set; }
+        public bool IsCheck { get; set; }
+    }
 }
